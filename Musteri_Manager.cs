04a65ea8fe6cdd63b25a3e113d815dcd376b0d5f
kadir03debug <70391809+kadir03debug@users.noreply.github.com>
2021-01-19 16:59:06 +0300

    Add files via upload

---
+++ Musteri_Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_demo
+{
+    class Musteri_Manager
+    {
+        public void Ekle() { Console.WriteLine("eklendi"); Console.ReadLine(); }
+        public void Sil() { Console.WriteLine("silindi"); Console.ReadLine(); }
+        public void Guncelle() { Console.WriteLine("güncellendi"); Console.ReadLine(); }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri Musteri_1 = new Musteri();
+            Musteri_Manager Manager = new Musteri_Manager();
+            Musteri_1.Musteri_Adi = "kadir";
+            Musteri_1.Musteri_Id = 0;
+            Musteri_1.Musteri_KİMLİKNo = "33333333";
+
+            Manager.Ekle();
+            Manager.Guncelle();
+            Manager.Sil();
+            
+        }
+    }
+}
